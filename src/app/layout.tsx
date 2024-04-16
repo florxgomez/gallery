ad51d5eb6a61a45a268fb29adec8c69be6f2b011
Florencia Gomez <florxgomez@gmail.com>
2024-04-16 17:56:54 -0300

    improve build

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -15,7 +15,7 @@ export const metadata = {
 
 function TopNav() {
   return (
-    <nav className="flex w-full items-center justify-between border-b p-4 text-xl font-semibold">
+    <nav className="flex w-full items-center justify-between border-b border-gray-600 p-4 text-xl font-semibold">
       <div>Gallery</div>
       <div>Sign In</div>
     </nav>
