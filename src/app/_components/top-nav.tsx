aac4bbed489d173f6fe6e1e75809963a2aeb1a70
Florencia Gomez <florxgomez@gmail.com>
2024-04-16 23:20:49 -0300

    use auth for home page

--- src/app/_components/top-nav.tsx
+++ src/app/_components/top-nav.tsx
@@ -1,10 +1,4 @@
-import {
-  SignInButton,
-  SignOutButton,
-  SignedIn,
-  SignedOut,
-  UserButton,
-} from "@clerk/nextjs";
+import { SignInButton, SignedIn, SignedOut, UserButton } from "@clerk/nextjs";
 
 export default function TopNav() {
   return (
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,24 +1,37 @@
+import { SignedIn, SignedOut } from "@clerk/nextjs";
 import { db } from "~/server/db";
 
 // we want to make this page dynamic because we want to make sure every time there's a change in our db, the page's content is updated
 export const dynamic = "force-dynamic";
 
-// sever component
-export default async function HomePage() {
+async function Images() {
   const images = await db.query.images.findMany({
     orderBy: (model, { desc }) => desc(model.id),
   });
+  return (
+    <div className="flex flex-wrap gap-4">
+      {images.map((image) => (
+        <div key={image.id} className=" flex w-48 flex-col">
+          <img src={image.url} alt="mock image" />
+          <div>{image.name}</div>
+        </div>
+      ))}
+    </div>
+  );
+}
 
+// sever component
+export default function HomePage() {
   return (
     <main className="">
-      <div className="flex flex-wrap gap-4">
-        {images.map((image) => (
-          <div key={image.id} className=" flex w-48 flex-col">
-            <img src={image.url} alt="mock image" />
-            <div>{image.name}</div>
-          </div>
-        ))}
-      </div>
+      <SignedOut>
+        <div className="h-full w-full text-center text-xl">
+          Please sign in above in order to proceed
+        </div>
+      </SignedOut>
+      <SignedIn>
+        <Images />
+      </SignedIn>
     </main>
   );
 }
