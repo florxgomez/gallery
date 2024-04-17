7c6c3dc6680d14fc07b52525889fc340f1f3f52a
Florencia Gomez <florxgomez@gmail.com>
2024-04-16 22:51:35 -0300

    linking db to the ui

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,34 +3,19 @@ import { db } from "~/server/db";
 // we want to make this page dynamic because we want to make sure every time there's a change in our db, the page's content is updated
 export const dynamic = "force-dynamic";
 
-const mockUrls = [
-  "https://utfs.io/f/45e48797-c5d5-4a6f-afd9-b5b0161809a7-1xf4e9.JPG",
-  "https://utfs.io/f/4c1b1854-f7a1-4ec7-9232-f0a105de80a0-63jlzi.jpeg",
-  "https://utfs.io/f/13de7535-f496-4769-bea1-4a77083df37a-1fqont.jpeg",
-  "https://utfs.io/f/8e141e42-fd8b-444f-8e9f-eef7ede8638e-1xf4d6.JPG",
-  "https://utfs.io/f/5c4bd7cf-69fa-463a-978d-6742a7808116-pi1muk.jpeg",
-  "https://utfs.io/f/3a096c0f-eb66-4ab7-b175-57f9e2bb67bd-1gtqtb.jpeg",
-];
-
-const mockImages = mockUrls.map((url, index) => ({
-  id: index + 1,
-  url,
-}));
-
 // sever component
 export default async function HomePage() {
-  const posts = await db.query.posts.findMany();
-  console.log(posts);
+  const images = await db.query.images.findMany({
+    orderBy: (model, { desc }) => desc(model.id),
+  });
 
   return (
     <main className="">
       <div className="flex flex-wrap gap-4">
-        {posts.map((post) => (
-          <div key={post.id}>{post.name}</div>
-        ))}
-        {mockImages.map((image) => (
-          <div key={image.id} className="w-48">
+        {images.map((image) => (
+          <div key={image.id} className=" flex w-48 flex-col">
             <img src={image.url} alt="mock image" />
+            <div>{image.name}</div>
           </div>
         ))}
       </div>
